0a75e509b7898f68e9a006a6058f79b427cd20ab
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2021-04-21 02:14:10 +0200

    Crosshair animation

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] private UnityEngine.CharacterController characterController;
+    
     [SerializeField] private Animator characterAnimator;
     [SerializeField] private Animator cameraAnimator;
+    [SerializeField] private Animator crosshairAnimator;
+    
     [SerializeField] private Vector3 playerVelocity;
     
     [SerializeField] private float currentSpeed;
@@ -14,52 +17,48 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
 
-    Vector2 rotation = new Vector2 (0, 0);
-    public float rotateSpeed = 3;
+    [SerializeField] private Vector2 rotation = new Vector2 (0, 0);
+    
+    
+    [SerializeField] private float currentRotateSpeed;
+    [SerializeField] private float zoomingRotateSpeed = 1f;
+    [SerializeField] private float normalRotateSpeed = 3f;
 
     void Update()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = false;
         
-        rotation.y += Input.GetAxis ("Mouse X");
-        rotation.x += -Input.GetAxis ("Mouse Y") / 1.5f;
-        transform.eulerAngles = rotation * rotateSpeed;
+        rotation.y += Input.GetAxis ("Mouse X") * currentRotateSpeed;
+        rotation.x += rotation.x - Input.GetAxis ("Mouse Y") * currentRotateSpeed / 1.5f > -35f 
+            && rotation.x - Input.GetAxis ("Mouse Y") * currentRotateSpeed / 1.5f < 35f 
+            ? -Input.GetAxis ("Mouse Y") * currentRotateSpeed / 1.5f
+            : 0f;
+        transform.eulerAngles = rotation;
         
         if (characterController.isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        if (Input.GetButtonDown("Jump") && characterController.isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
         
-        if (Input.GetKey(KeyCode.W))
-        {
-            characterController.Move(transform.forward * (Time.deltaTime * currentSpeed));
-        }
+        if (Input.GetKey(KeyCode.W)) characterController.Move(transform.forward * (Time.deltaTime * currentSpeed));
+        if (Input.GetKey(KeyCode.S)) characterController.Move(transform.forward * (Time.deltaTime * -currentSpeed));
+        if (Input.GetKey(KeyCode.A)) characterController.Move(transform.right * (Time.deltaTime * -currentSpeed));
+        if (Input.GetKey(KeyCode.D)) characterController.Move(transform.right * (Time.deltaTime * currentSpeed));
+
+        currentSpeed = !Input.GetKey(KeyCode.Mouse1) && Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        currentRotateSpeed = Input.GetKey(KeyCode.Mouse1) ? zoomingRotateSpeed : normalRotateSpeed;
         
-        if (Input.GetKey(KeyCode.S))
-        {
-            characterController.Move(transform.forward * (Time.deltaTime * -currentSpeed));
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            characterController.Move(transform.right * (Time.deltaTime * -currentSpeed));
-        }
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            characterController.Move(transform.right * (Time.deltaTime * currentSpeed));
-        }
-        
-        currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
-        characterAnimator.SetFloat("runningMultiplier", Input.GetKey(KeyCode.LeftShift) ? 1.5f : 1f);
+        characterAnimator.SetFloat("runningMultiplier", !Input.GetKey(KeyCode.Mouse1) && Input.GetKey(KeyCode.LeftShift) ? 1.75f : 1f);
         characterAnimator.SetBool("isMoving", Mathf.Abs(characterController.velocity.x) > 0.5f || Mathf.Abs(characterController.velocity.z) > 0.5f);
         cameraAnimator.SetBool("isZooming", Input.GetKey(KeyCode.Mouse1));
+        crosshairAnimator.SetBool("isZooming", Input.GetKey(KeyCode.Mouse1));
+        
         playerVelocity.y += gravityValue * Time.deltaTime;
         characterController.Move(playerVelocity * Time.deltaTime);
     }
